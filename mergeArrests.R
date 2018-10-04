bc2dc3f25d29b5da3cfccbcbbbd88f1cc385babd
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-04 11:56:38 -0400

    Saving mergeArrests.R

--- mergeArrests.R
+++ mergeArrests.R
@@ -1,7 +1,7 @@
 
 #assigning USArrests dataframe to arrests variable
 arrests<-USArrests 
-row.names(data)<-data["stateName"]
+# row.names(data)<-data["stateName"]
 mergedData<-merge(data,arrests,by="row.names",all=TRUE)
 mergedData
 
