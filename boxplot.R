52dadb101399ad357addb9d2dbea7c82692c7dce
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-08 22:16:27 -0400

    Saving boxplot.R

--- boxplot.R
+++ boxplot.R
@@ -1,6 +1,8 @@
 
+#loading the ggplot2 library
 library(ggplot2)
 boxpop<-ggplot(data)+aes(y=population)+geom_boxplot()
 boxmurder<-ggplot(data)+aes(y=Murder)+geom_boxplot()
 
 #seeing both histograms and boxplots, box plot makes more sense of the data as we could clearly see the outliers in the population.
+# If we want to see the individual data of each state then histograms make more sense as, you can see individual values
