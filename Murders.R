1fca9db558093d3a3ad2c6ec571a22457cad4c15
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-08 23:52:27 -0400

    Saving Murders.R

--- Murders.R
+++ Murders.R
@@ -2,4 +2,5 @@
 library(ggplot2)
 data$murdersPerState<-data$Murder*data$population/100000
 barmurder<-ggplot(data)+aes(x=stateName,y=data$murdersPerState)+geom_col()
-barmurder2<-barmurder+theme(axis.text.x=element_text(angle = 90, hjust = 1))
+barmurder1<-barmurder+theme(axis.text.x=element_text(angle = 90, hjust = 1))
+barmurder2<-ggplot(data[order(data$Murder),])+aes(x=stateName,y=data$murdersPerState)+geom_col()+theme(axis.text.x=element_text(angle = 90, hjust = 1))
