549256130736d5c5f46c87c9db4bc1f34d9d69ec
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-08 22:15:23 -0400

    Saving boxplot.R

--- boxplot.R
+++ boxplot.R
@@ -2,3 +2,5 @@
 library(ggplot2)
 boxpop<-ggplot(data)+aes(y=population)+geom_boxplot()
 boxmurder<-ggplot(data)+aes(y=Murder)+geom_boxplot()
+
+#seeing both histograms and boxplots, box plot makes more sense of the data as we could clearly see the outliers in the population.
