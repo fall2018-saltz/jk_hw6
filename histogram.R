fbcc5d3fa16acb182cf88abcc0ed99c5f3ec23c9
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-08 20:28:11 -0400

    Saving histogram.R

--- histogram.R
+++ histogram.R
@@ -1,4 +1,4 @@
 
 library(ggplot2)
-pophist<-ggplot(data)+aes(x=stateName,y=population)+geom_histogram(stat="identity")+theme(axis.text.x=element_text(angle = -90, hjust = 0))
-murderhist<-ggplot(data)+aes(x=stateName,y=Murder)+geom_histogram(stat="identity")+theme(axis.text.x=element_text(angle = -90, hjust = 0))
+pophist<-ggplot(data)+aes(x=stateName,y=population)+geom_histogram(stat="identity")+theme(axis.text.x=element_text(angle = 90, hjust = 0))
+murderhist<-ggplot(data)+aes(x=stateName,y=Murder)+geom_histogram(stat="identity")+theme(axis.text.x=element_text(angle = 90, hjust = 0))
