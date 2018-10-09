afbf7df695a3c5959188badcb2f3e2bd1b9f81b6
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-08 20:00:14 -0400

    Saving histmurder.R

--- histmurder.R
+++ histmurder.R
@@ -1,3 +1,3 @@
 
 library(ggplot2)
-murderhist<-ggplot(data)+aes(x=stateName,y=population)+geom_histogram(stat="identity")
+murderhist<-ggplot(data)+aes(x=stateName,y=murder)+geom_histogram(stat="identity")
