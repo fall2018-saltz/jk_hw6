be6ff61b1eed596d95b697b3455cb31714b7bb99
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-08 22:21:52 -0400

    Saving histogram.R

--- histogram.R
+++ histogram.R
@@ -1,8 +1,9 @@
 
 #loading the ggplot2 library
 library(ggplot2)
-#creating the histogram of population of various states
+#creating the histogram of population of various states where theme element is used to adjust the labels
 pophist<-ggplot(data)+aes(x=stateName,y=population)+geom_histogram(stat="identity")+theme(axis.text.x=element_text(angle = 90, hjust = 1))
+#creating the murder rate histogram of various states where theme element is used to adjust the labels
 murderhist<-ggplot(data)+aes(x=stateName,y=Murder)+geom_histogram(stat="identity")+theme(axis.text.x=element_text(angle = 90, hjust = 1))
 
 #seeing both histograms and boxplots, box plot makes more sense of the data as we could clearly see the outliers in the population.
