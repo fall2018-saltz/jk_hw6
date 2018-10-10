66d47fb223029956bd5321cca9ae5c1b187a3ea3
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 17:33:40 -0400

    Saving mergeArrests.R

--- mergeArrests.R
+++ mergeArrests.R
@@ -1,8 +1,11 @@
 
 #assigning USArrests dataframe to arrests variable
 arrests<-USArrests 
+
 #removing the district of Columbia data which is not available in US arrests.
 data<-data[-c(which(data$stateName=="District of Columbia")),]
+
+#changing the rownames of data to it's column stateName for common merge element
 rownames(data)<-as.vector(data[,'stateName'])
 mergedData<-merge(data,arrests,by="row.names",all=TRUE)
 
