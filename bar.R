afdf0de00ef732778a051eae4d0c4936dbb86432
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 17:27:58 -0400

    Saving bar.R

--- bar.R
+++ bar.R
@@ -3,8 +3,12 @@
 library(ggplot2)
 #adding a new column to data which is total number of murders per state as murdersPerState
 data$murdersPerState<-data$Murder*data$population/100000
-#generating barchart
+
+#generating a bar chart, with the number of murders per state
 barmurder<-ggplot(data)+aes(x=stateName,y=murdersPerState)+geom_col()+ggtitle("Total Murders")
+
 barmurder1<-barmurder+theme(axis.text.x=element_text(angle = 90, hjust = 1))
+
 barmurder2<-ggplot(data[order(data$Murder),])+aes(x=stateName,y=data$murdersPerState)+geom_col()+theme(axis.text.x=element_text(angle = 90, hjust = 1))
+
 barmurder3<-ggplot(data[order(data$Murder),])+aes(x=stateName,y=data$murdersPerState,color=data$percentOver18)+geom_col()+theme(axis.text.x=element_text(angle = 90, hjust = 1))
