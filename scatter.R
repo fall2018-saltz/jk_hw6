89eba1ad1b5d67d569962e910c1d0d5581322e9f
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 17:31:29 -0400

    Saving scatter.R

--- scatter.R
+++ scatter.R
@@ -2,4 +2,5 @@
 #loading the ggplot2 library
 library(ggplot2)
 
+#Generating a scatter plot – having population on the X axis, the percent over 18 on the y axis, and the size & color represent the murder rate
 scatter<-ggplot(data)+aes(x=population,y=percentOver18,size=Murder,color=Murder)+geom_point()
