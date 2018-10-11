c7a71f397ebde2a8099b27efca23b4e240b9f1af
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 21:55:02 -0400

    Saving histogram.R

--- histogram.R
+++ histogram.R
@@ -9,4 +9,6 @@ murderhist<-ggplot(data)+aes(x=stateName,y=Murder)+geom_histogram(stat="identity
 #seeing both histograms and boxplots, box plot makes more sense of the data as we could clearly see the outliers in the population.
 # If we want to see the individual data of each state then histograms make more sense as, you can see individual values
 pophist<-ggplot(data)+aes(x=stateName,y=population)+geom_histogram(stat="identity")+theme(axis.text.x=element_text(angle = 90, hjust = 1))
+pophist<-ggplot(data)+aes(x=stateName,y=population)+geom_histogram(stat="identity")+theme(axis.text.x=element_text(angle = 90, hjust = 1))
+pophist<-ggplot(data)+aes(x=stateName,y=population)+geom_histogram(stat="identity")+theme(axis.text.x=element_text(angle = 90, hjust = 1))
 #creating histograms of other remaining 5 variables
